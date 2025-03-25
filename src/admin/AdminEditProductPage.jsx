198438c589cbb6764c3ac9b501e3adb6858aed06
kannank16 <kannankaveriyappan5@gmail.com>
2025-03-25 08:27:40 +0530

    updated

--- src/admin/AdminEditProductPage.jsx
+++ src/admin/AdminEditProductPage.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Form, Button, CloseButton, Table,Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const AdminEditProductPage = () => {
-  return (
-    <div>AdminEditProductPage</div>
-  )
-}
+  const [validated, setValidated] = useState(false);
 
-export default AdminEditProductPage
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <div>
+      <Row className="mt-5 justify-content-md-center ">
+        <Col md={1}>
+          <Link
+            to={"/admin/product"}
+            className="btn btn-info my-3 text-decoration-none text-white"
+          >
+            {" "}
+            Go Back
+          </Link>
+
+          {/* <Button></Button> */}
+        </Col>
+        <Col md={6}>
+          <h1>Create New Product</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name="name" required />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" rows={3} required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="FormBasicCount">
+              <Form.Label>Count in Stock</Form.Label>
+              <Form.Control type="number" name="count" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="FormBasisPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="text" name="price" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="FormBasisPrice">
+              <Form.Label>
+                category{"  "}
+                <CloseButton /> <small>(remove selected)</small>
+              </Form.Label>
+              <Form.Select aria-label="Default select example" required>
+                <option>Choose one Category</option>
+                <option value="1">Laptops</option>
+                <option value="2">Tv</option>
+                <option value="3">Games</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicCategory" className="mb-3 mt-3">
+              <Form.Label>
+                or create a new category(example computer/laptops)
+              </Form.Label>
+              <Form.Control type="text" required />
+            </Form.Group>
+
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="FormBasisAttribute">
+                  <Form.Label>Choose new attribute</Form.Label>
+                  <Form.Select aria-label="Default select example" required>
+                    <option>Choose attribute </option>
+                    <option value="1">color</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                {" "}
+                <Form.Group className="mb-3" controlId="FormBasisPrice">
+                  <Form.Label>Choose Attribute Value{"  "}</Form.Label>
+                  <Form.Select aria-label="Default select example" required>
+                    <option>Choose value</option>
+                    <option value="1">green</option>
+                    <option value="2">blue</option>
+                    <option value="3">red</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Attribute</th>
+                  <th>Value</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>attrkey</td>
+                  <td>value</td>
+                  <td>
+                    <CloseButton></CloseButton>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+            <Row>
+              <Col md={6}>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Choose New Attribute</Form.Label>
+              <Form.Control type="text" name="name" required placeholder="first choose or create category"/>
+            </Form.Group>
+              </Col>
+              <Col md={6}>
+                {" "}<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>choose new attribute value</Form.Label>
+              <Form.Control type="text" name="name" required placeholder="first choice or create category"/>
+            </Form.Group>
+              </Col>
+            </Row>
+            <Alert variant='info'>After Typing Attribute key and Value press on enter field</Alert>
+            <Form.Group controlId="formFile" className="mb-3 mt-3">
+              <Form.Label>Images</Form.Label>
+              <Form.Control type="file" required />
+            </Form.Group>
+
+            <Form.Group controlId="formFile" className="mb-3 ">
+              <Button type="submit" variant="primary">
+                create
+              </Button>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default AdminEditProductPage;
--- src/admin/AdminProductPage.jsx
+++ src/admin/AdminProductPage.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Table, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import AdminLinkComponent from "../components/admin/AdminLinkComponent";
 
 const AdminProductPage = () => {
+  const handleDelete=()=>{
+    if(window.confirm('are you sure ?')) alert('product deleted')
+  }
   return (
-    <div>AdminProductPage</div>
-  )
-}
+    <div>
+      <Row className="m-5">
+        <Col md={2}>
+          <AdminLinkComponent />
+        </Col>
+        <Col md={10}>
+          <h1>
+            Products list
+            {"  "}
+            <Button
+              variant="primary"
+              size="lg"
+              as={Link}
+              to={"/admin/create-product"}
+            >
+              Create New
+            </Button>
+          </h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>S.No</th>
+                <th>Product Name </th>
+                <th>Price</th>
+                <th>Category</th>
+                <th>Edit/Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {[
+                { name: "panasonic", price: "190$", category: "Tv" },
+                { name: "dell", price: "120$", category: "Laptop" },
+                { name: "Gta-5", price: "50$", category: "Games" },
+              ].map((item, idx) => (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.category}</td>
+                  <td>
+                    <Button variant="primary" size="small" as={Link} to={'/admin/edit-product'}>
+                    <i className="bi bi-pencil-square" ></i>
 
-export default AdminProductPage
+                    </Button> {" / "}
+                    
+                    <Button variant="danger" size="small" onClick={handleDelete}>
+
+                    <i className="bi bi-x-circle"></i>
+
+                    </Button> 
+                  </td>
+                  
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default AdminProductPage;
